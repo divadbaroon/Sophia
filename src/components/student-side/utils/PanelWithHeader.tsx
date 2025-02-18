a0a202bec281944c5b641b2ef0bda3089d7c2a98
David <dbarron410@vt.edu>
2025-02-18 08:22:23 -0500

    init

---
+++ src/components/student-side/utils/PanelWithHeader.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+interface PanelWithHeaderProps {
+  children: React.ReactNode;
+}
+
+export const PanelWithHeader = ({ children }: PanelWithHeaderProps) => (
+  <div className="h-full flex flex-col">
+    <div className="h-24 bg-white"></div>  
+    <div className="flex-1">
+      {children}
+    </div>
+  </div>
+);
