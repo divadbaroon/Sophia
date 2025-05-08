7c9ac84673b8d82c1135ee84c8189c28a4e26ebe
David <dbarron410@vt.edu>
2025-05-08 18:06:16 -0400

    anomymize name

--- src/app/(root)/sessions/[sessionId]/[studentId]/page.tsx
+++ src/app/(root)/sessions/[sessionId]/[studentId]/page.tsx
@@ -31,7 +31,7 @@ export default function StudentSessionDashboard() {
   }
 
   const sessionData = {
-    studentName: "David Barron",
+    studentName: "Student",
     sessionTime: "15:30",
     waitTime: "1 min",
     courseInfo: {
