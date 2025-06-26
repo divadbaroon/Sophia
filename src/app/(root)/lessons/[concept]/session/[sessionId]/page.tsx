fb213ddd5fa9bdefcf1deb44c2b5fdd8aa9216c3
David <dbarron410@vt.edu>
2025-06-26 01:01:17 -0700

    remove toaster

--- src/app/(root)/lessons/[concept]/session/[sessionId]/page.tsx
+++ src/app/(root)/lessons/[concept]/session/[sessionId]/page.tsx
@@ -4,7 +4,6 @@ import React from 'react';
 import { WorkspaceLayout } from '@/components/student-side/work-pace/WorkSpace';
 import { SophiaBrainProvider } from '@/components/student-side/voice-chat/SophiaBrainProvider'
 import { FileProvider } from '@/lib/context/FileContext';
-import { Toaster } from 'sonner';
 
 const JoinSessionPage: React.FC = () => {
 
@@ -12,7 +11,6 @@ const JoinSessionPage: React.FC = () => {
       <FileProvider>
         <SophiaBrainProvider>
           <WorkspaceLayout />
-        <Toaster position="top-right" />
         </SophiaBrainProvider>
       </FileProvider>
   );
