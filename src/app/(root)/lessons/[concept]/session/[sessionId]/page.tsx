fb32f0beb6b26c0d06f5f542dff97227732e6f48
David <dbarron410@vt.edu>
2025-06-25 22:46:55 -0700

    now wrapping workspace in SophiaBrainProvider

--- src/app/(root)/lessons/[concept]/session/[sessionId]/page.tsx
+++ src/app/(root)/lessons/[concept]/session/[sessionId]/page.tsx
@@ -2,6 +2,7 @@
 
 import React from 'react';
 import { WorkspaceLayout } from '@/components/student-side/work-pace/WorkSpace';
+import { SophiaBrainProvider } from '@/components/student-side/voice-chat/SophiaBrainProvider'
 import { FolderProvider } from '@/components/context/FolderContext';
 import { FileProvider } from '@/lib/context/FileContext';
 import { Toaster } from 'sonner';
@@ -11,8 +12,10 @@ const JoinSessionPage: React.FC = () => {
   return (
     <FolderProvider>
       <FileProvider>
-        <WorkspaceLayout />
+        <SophiaBrainProvider>
+          <WorkspaceLayout />
         <Toaster position="top-right" />
+        </SophiaBrainProvider>
       </FileProvider>
     </FolderProvider>
   );
