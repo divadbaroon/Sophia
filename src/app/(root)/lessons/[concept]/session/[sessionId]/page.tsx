993e83d076bc0fe8b37fdb46979b891bbc6468a7
David <dbarron410@vt.edu>
2025-06-26 01:00:07 -0700

    remove folder provider

--- src/app/(root)/lessons/[concept]/session/[sessionId]/page.tsx
+++ src/app/(root)/lessons/[concept]/session/[sessionId]/page.tsx
@@ -3,21 +3,18 @@
 import React from 'react';
 import { WorkspaceLayout } from '@/components/student-side/work-pace/WorkSpace';
 import { SophiaBrainProvider } from '@/components/student-side/voice-chat/SophiaBrainProvider'
-import { FolderProvider } from '@/components/context/FolderContext';
 import { FileProvider } from '@/lib/context/FileContext';
 import { Toaster } from 'sonner';
 
 const JoinSessionPage: React.FC = () => {
 
   return (
-    <FolderProvider>
       <FileProvider>
         <SophiaBrainProvider>
           <WorkspaceLayout />
         <Toaster position="top-right" />
         </SophiaBrainProvider>
       </FileProvider>
-    </FolderProvider>
   );
 };
 
