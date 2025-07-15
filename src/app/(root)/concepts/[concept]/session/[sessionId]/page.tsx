76f1703cf22347c97d9ae020feeb97670afca392
David <dbarron410@vt.edu>
2025-07-14 17:59:40 -0700

    remove SophiaBrainProvider for now

--- src/app/(root)/concepts/[concept]/session/[sessionId]/page.tsx
+++ src/app/(root)/concepts/[concept]/session/[sessionId]/page.tsx
@@ -2,16 +2,13 @@
 
 import React from 'react';
 import { WorkspaceLayout } from '@/components/student-side/work-pace/WorkSpace';
-import { SophiaBrainProvider } from '@/components/student-side/voice-chat/provider/SophiaBrainProvider'
 import { FileProvider } from '@/lib/context/FileContext';
 
 const JoinSessionPage: React.FC = () => {
 
   return (
       <FileProvider>
-        <SophiaBrainProvider>
           <WorkspaceLayout />
-        </SophiaBrainProvider>
       </FileProvider>
   );
 };
