acbe1e47874895cf3b4bfee59079d0407c1afde5
David <dbarron410@vt.edu>
2025-07-15 21:33:30 -0700

    remove unneeded files

--- src/components/student-side/voice-chat/context/SophiaBrainContext.ts
+++
@@ -1,5 +0,0 @@
-import { createContext } from 'react'
-import { SophiaBrainController } from '../types/SophiaBrainType'
-
-// Create context
-export const SophiaBrainContext = createContext<SophiaBrainController | null>(null)
