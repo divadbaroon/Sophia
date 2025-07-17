b3fd9f636ee32285d4f0b0e037775de839e0685f
David <dbarron410@vt.edu>
2025-07-16 20:44:37 -0700

    remove task provider

--- src/app/(root)/concepts/[concept]/session/[sessionId]/page.tsx
+++ src/app/(root)/concepts/[concept]/session/[sessionId]/page.tsx
@@ -3,17 +3,15 @@
 import React from 'react';
 import { WorkspaceLayout } from '@/components/student-side/work-pace/WorkSpace';
 
-import { SessionProvider, CodeEditorProvider, TaskProgressProvider, ConceptMapProvider  } from '@/lib/context';
+import { SessionProvider, CodeEditorProvider, ConceptMapProvider  } from '@/lib/context';
 
 const JoinSessionPage = () => {
   return (
     <SessionProvider>
       <CodeEditorProvider>
-        <TaskProgressProvider>
           <ConceptMapProvider>
               <WorkspaceLayout />
           </ConceptMapProvider>
-        </TaskProgressProvider>
       </CodeEditorProvider>
     </SessionProvider>
   );
