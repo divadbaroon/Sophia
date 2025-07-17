270caf23e218b964f8367c6fdf424a1fb19154ad
David <dbarron410@vt.edu>
2025-07-16 18:32:58 -0700

    remove quiz provider

--- src/lib/context/index.ts
+++ src/lib/context/index.ts
@@ -2,4 +2,3 @@ export { SessionProvider, useSession } from './session/SessionProvider';
 export { CodeEditorProvider, useCodeEditor } from './codeEditor/CodeEditorProvider';
 export { TaskProgressProvider, useTaskProgress } from './taskProgress/TaskProgressProvider';
 export { ConceptMapProvider, useConceptMap } from './conceptMap/ConceptMapProvider';
-export { QuizProvider, useQuiz } from './quiz/QuizProvider';
