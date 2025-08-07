39d14c601a8dc370617e706176f781078183250c
David <dbarron410@vt.edu>
2025-08-07 19:37:21 -0400

    add ConversationProvider

--- src/lib/context/index.ts
+++ src/lib/context/index.ts
@@ -1,3 +1,4 @@
 export { SessionProvider, useSession } from './session/SessionProvider';
 export { CodeEditorProvider, useCodeEditor } from './codeEditor/CodeEditorProvider';
+export { ConversationProvider, useConversation } from './conversation/conversationHistoryProvider'
 export { ConceptMapProvider, useConceptMap } from './conceptMap/ConceptMapProvider';
