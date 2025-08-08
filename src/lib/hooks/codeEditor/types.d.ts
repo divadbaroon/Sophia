93125ab962e7fa8ec6b71b88d088087946198276
David <dbarron410@vt.edu>
2025-08-07 23:40:55 -0400

    add UseAutoSaveProps

--- src/lib/hooks/codeEditor/types.d.ts
+++ src/lib/hooks/codeEditor/types.d.ts
@@ -2,3 +2,10 @@ export interface UseCodeSnapshotsReturn {
   initialMethodsCode: Record<string, string>;
   isLoading: boolean;
 }
+
+export interface UseAutoSaveProps {
+  activeMethodId: string | undefined
+  sessionId: string | undefined
+  lessonId: string | undefined
+  currentMethodIndex: number | undefined
+}
