288a65596cdf71029cfef4c45e45f0fdde6a385b
David <dbarron410@vt.edu>
2025-08-09 01:26:19 -0400

    remove useTrackProgress hook

--- src/lib/hooks/taskProgress/types.d.ts
+++
@@ -1,7 +0,0 @@
-export interface UseTaskProgressReturn {
-  completedTasks: Set<number>
-  isTaskCompleted: (taskIndex: number) => boolean
-  isLoading: boolean
-  error: string | null
-  refreshTaskProgress: () => Promise<void>
-}
